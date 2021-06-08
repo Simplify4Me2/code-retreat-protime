c9137072784388f72f98984f577591448a04a36d
Simon Vereecke <simon.vereecke@protime.eu>
2021-06-08 16:33:40 +0200

    foo

---
+++ ConsoleApp2/TestProject2/Board.cs
@@ -0,0 +1,11 @@
+﻿namespace TestProject2
+{
+    internal class Board
+    {
+        public Board()
+        {
+        }
+
+        public object Moves { get; internal set; }
+    }
+}
---
+++ ConsoleApp2/TestProject2/Player.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TestProject2
+{
+    internal class Player
+    {
+        public Player()
+        {
+        }
+
+        internal void MakesMove(object board)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
