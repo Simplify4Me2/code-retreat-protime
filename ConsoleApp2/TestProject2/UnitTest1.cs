d861234e354a09dc5284bd5d2c816461deeefb9d
Simon Vereecke <simon.vereecke@protime.eu>
2021-06-08 16:47:23 +0200

    added ctor

--- ConsoleApp2/TestProject2/UnitTest1.cs
+++ ConsoleApp2/TestProject2/UnitTest1.cs
@@ -1,11 +1,17 @@
 using FluentAssertions;
 using System;
 using Xunit;
+using Moq;
 
 namespace TestProject2
 {
     public class UnitTest1
     {
+
+        public UnitTest1()
+        {
+
+        }
         
         [Fact]
         public void Test1()
